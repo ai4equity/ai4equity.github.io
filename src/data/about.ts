586346eb26a552ecc336bf42a2dec6109f3969fb
ai4equity <oneflowers9@gmail.com>
2026-06-27 15:07:28 -0400

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -32,10 +32,10 @@ My research also extends to motivation, learning analytics, and problem-based le
 
 # Funded Work
 
-- Bridging the AI Adoption Gap: Teacher-Led Design and Implementation of Generative AI-Supported Mathematics Instruction, Anne Spencer Daves College of Education, Health, and Human Sciences, Florida State University, Dec 2025-Mar 2027. Total award $25,213.59.
-- Advancing Equity in Online Learning: Examination of an Open-Source Chatbot Enhanced by Customized Datasets and Open Educational Resources, First Year Assistant Professor Grant, Florida State University, 2025. Total award $20,000.
-- Large Language Model-Powered Retrieval-Augmented Generation Chatbot and Dashboard Pipelines: Scalable Solutions for Students' Academic Success, FSU / AWS Funding for Computer Support Seed Fund, 2025-2027. Total award $20,000.
-- Launching AI4Equity Lab, Anne Spencer Daves College of Education, Health, and Human Sciences, Florida State University, 2024-2025. Total award $9,950.
+- Bridging the AI Adoption Gap: Teacher-Led Design and Implementation of Generative AI-Supported Mathematics Instruction, PI role, Anne Spencer Daves College of Education, Health, and Human Sciences, Florida State University, Dec 2025-Mar 2027. Total award $25,213.59.
+- Advancing Equity in Online Learning: Examination of an Open-Source Chatbot Enhanced by Customized Datasets and Open Educational Resources, PI role, First Year Assistant Professor Grant, Florida State University, 2025. Total award $20,000.
+- Large Language Model-Powered Retrieval-Augmented Generation Chatbot and Dashboard Pipelines: Scalable Solutions for Students' Academic Success, PI role, FSU / AWS Funding for Computer Support Seed Fund, 2025-2027. Total award $20,000.
+- Launching AI4Equity Lab, Director role, Anne Spencer Daves College of Education, Health, and Human Sciences, Florida State University, 2024-2025. Total award $9,950.
 
 # Teaching
 
